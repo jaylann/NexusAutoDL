2388ca94bd1890f4e5b34a9ff75b60626ba2b319
Jaylann <Justin.Lanfermann@hotmail.com>
2022-11-10 16:12:50 +0100

    refactor

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+import ctypes
+import os
 import subprocess
 import time
 
-import ctypes
-import win32api, win32con
-import numpy as np
 import cv2
+import mss
+import numpy as np
+import win32api
+import win32con
 
 user32 = ctypes.windll.user32
-import os
-import mss
 
 
 class System:
@@ -91,28 +92,28 @@ class System:
                     time.sleep(5)
             time.sleep(2)
 
-    def click(self, x, y):
+    @staticmethod
+    def click(x, y):
         o_pos = win32api.GetCursorPos()
         win32api.SetCursorPos((x, y))
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
         win32api.SetCursorPos(o_pos)
 
+    def prep_chrome(self):
+        subprocess.Popen(r'start chrome /new-tab about:blank', shell=False)
+        time.sleep(0.4)
 
-def moveWindows(monitors):
-    subprocess.Popen(r'start chrome /new-tab about:blank', shell=False)
-    time.sleep(0.4)
-
-    chrome = user32.FindWindowW(None, u"about:blank - Google Chrome")
-    vortex = user32.FindWindowW(None, u"Vortex")
-    user32.ShowWindow(chrome, 1)
-    user32.ShowWindow(vortex, 1)
-    if len(monitors) > 1:
-        x_c, y_c, w_c, h_c = monitors[0][0], monitors[0][1], monitors[0][2], monitors[0][3]
-        x_v, y_v, w_v, h_v = monitors[1][0], monitors[1][1], monitors[1][2], monitors[1][3]
-    else:
-        x_c, y_c, w_c, h_c = 0, 0, monitors[0][2] / 2, monitors[0][3] / 2
-        x_v, y_v, w_v, h_v = monitors[0][2] / 2, monitors[0][3] / 2, monitors[0][2], monitors[0][3]
-    user32.moveWindow(chrome, x_c, y_c, w_c, h_c, True)
-    user32.moveWindow(vortex, x_v, y_v, w_v, h_v, True)
-
+        chrome = user32.FindWindowW(None, u"about:blank - Google Chrome")
+        vortex = user32.FindWindowW(None, u"Vortex")
+        user32.ShowWindow(chrome, 1)
+        user32.ShowWindow(vortex, 1)
+        if len(self.monitors) > 1:
+            x_c, y_c, w_c, h_c = self.monitors[0][0], self.monitors[0][1], self.monitors[0][2], self.monitors[0][3]
+            x_v, y_v, w_v, h_v = self.monitors[1][0], self.monitors[1][1], self.monitors[1][2], self.monitors[1][3]
+        else:
+            x_c, y_c, w_c, h_c = 0, 0, self.monitors[0][2] / 2, self.monitors[0][3] / 2
+            x_v, y_v, w_v, h_v = self.monitors[0][2] / 2, self.monitors[0][3] / 2, self.monitors[0][2], \
+                                 self.monitors[0][3]
+        user32.moveWindow(chrome, x_c, y_c, w_c, h_c, True)
+        user32.moveWindow(vortex, x_v, y_v, w_v, h_v, True)
